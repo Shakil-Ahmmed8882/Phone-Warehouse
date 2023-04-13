1b6a374c717047b36e5bebe98bcd6ba61c8aff07
Shakil-Ahmmed8882 <127662337+Shakil-Ahmmed8882@users.noreply.github.com>
2023-04-13 03:48:38 -1200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num1 = parseInt(prompt('Enter your first number'))
+let num2 = parseInt (prompt('Enter your second number'))
+
+
+let sum = 0
+
+
+for (let i = num1 ;i <= num2; i++){
+    sum += i
+
+}
+
+
+document.write('sum' + sum)
